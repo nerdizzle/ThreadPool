17dfe6a89f251772f0a31133ba407d1fbe4ae2e6
pygmalion <m@h>
2019-11-12 21:57:44 +0100

    initial commit

---
+++ src/ThreadPool.h
@@ -0,0 +1,13 @@
+//
+// Created by pygmalion on 12.11.19.
+//
+
+#ifndef THREADPOOL_THREADPOOL_H
+#define THREADPOOL_THREADPOOL_H
+class ThreadPool {
+private:
+    int nThreads;
+public:
+    ThreadPool
+};
+#endif //THREADPOOL_THREADPOOL_H
